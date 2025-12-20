f2fa682711fcdeea8a1832e4857ced1f86a0ac99
Adrian <adrian.molenda069@gmail.com>
2025-12-20 18:31:18 +0100

    Fixxed inconsitent managment on stacked collapse within the update of spacing.

--- matt/static/js.js
+++ matt/static/js.js
@@ -983,19 +983,11 @@ $(function () {
             }
             //Resets state for concat collapse
             const temptree = getTree(child);
-            temptree.push(parent[side])
+            temptree.push(parent[side]);
 
             temptree.forEach(ci => {
-                if(collapsedmap[ci]["left"]){
-                    collapsedmap[ci]["left"] = false;
-                    svg.selectAll(`[data-id='${ci}']`).attr({stroke: "red"});
-                }
-                if(collapsedmap[ci]["right"]){
-                    collapsedmap[ci]["right"] = false;
-                    svg.selectAll(`[data-id='${ci}']`).attr({stroke: "red"});
-
-                }
-
+                if(collapsedmap[ci]["left"]) collapsedmap[ci]["left"] = false;
+                if(collapsedmap[ci]["right"]) collapsedmap[ci]["right"] = false;
             });
 
             // COLAPSE
@@ -1022,15 +1014,16 @@ $(function () {
                     minimap.selectAll(`path[data-id='${child}']`).attr({display: "inline"});
 
                 });
-               //TODO: FIX SPACING: DRAWING WRONG BUT STATES ARE CORRECT
-              getTree(start).forEach(child => {
-                  if(collapsedmap[child]["l-d"]) svg.select(`path[data-id='${collapsedmap[child]["left_id"]}']`).attr({d: collapsedmap[child]["l-d"], stroke: "red"});
-                  if(collapsedmap[child]["r-d"]) svg.select(`path[data-id='${collapsedmap[child]["right_id"]}']`).attr({d: collapsedmap[child]["r-d"], stroke: "red"});
-                  if(collapsedmap[child]["l_edge-Y"]) svg.select(`circle[data-id='${child}'][data-direct='left']`).attr({cy: collapsedmap[child]["l_edge-Y"], "data-v":collapsedmap[child]["l_edge-Y"]});
-                  if(collapsedmap[child]["r_edge-Y"]) svg.select(`circle[data-id='${child}'][data-direct='right']`).attr({cy: collapsedmap[child]["r_edge-Y"], "data-v":collapsedmap[child]["r_edge-Y"]});
-                  if(collapsedmap[child]["r-d"]) minimap.select(`path[data-child='${collapsedmap[child]["right_id"]}`).attr({d: collapsedmap[child]["minimap-r-m"]});
-                  if(collapsedmap[child]["l-d"]) minimap.select(`path[data-child='${collapsedmap[child]["left_id"]}']`).attr({d: collapsedmap[child]["minimap-r-m"]});
-              });
+                const temptree = getTree(child);
+                temptree.push(parent[side]);
+                temptree.forEach(child => {
+                    if(collapsedmap[child]["l-d"]) svg.select(`path[data-id='${collapsedmap[child]["left_id"]}']`).attr({d: collapsedmap[child]["l-d"], stroke: "red"});
+                    if(collapsedmap[child]["r-d"]) svg.select(`path[data-id='${collapsedmap[child]["right_id"]}']`).attr({d: collapsedmap[child]["r-d"], stroke: "red"});
+                    if(collapsedmap[child]["l_edge-Y"]) svg.select(`circle[data-id='${child}'][data-direct='left']`).attr({cy: collapsedmap[child]["l_edge-Y"], "data-v":collapsedmap[child]["l_edge-Y"]});
+                    if(collapsedmap[child]["r_edge-Y"]) svg.select(`circle[data-id='${child}'][data-direct='right']`).attr({cy: collapsedmap[child]["r_edge-Y"], "data-v":collapsedmap[child]["r_edge-Y"]});
+                    if(collapsedmap[child]["r-d"]) minimap.select(`path[data-child='${collapsedmap[child]["right_id"]}`).attr({d: collapsedmap[child]["minimap-r-m"]});
+                    if(collapsedmap[child]["l-d"]) minimap.select(`path[data-child='${collapsedmap[child]["left_id"]}']`).attr({d: collapsedmap[child]["minimap-r-m"]});
+                });
 
 
                 draw_collapsed_line(childitem, start, collapsed_check, direct);
